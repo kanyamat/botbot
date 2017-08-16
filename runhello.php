6b2f69e7db225b2715cdb5c0dfa33f317cf738aa
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-16 00:05:17 -0700

    Update runhello.php

--- runhello.php
+++ runhello.php
@@ -2,7 +2,7 @@
 
 {
     "displayName":"LINE taro",
-    "userId":"Uxxxxxxxxxxxxxx...",
+    "userId":"Ub840b452d253f3db490dd59507ab78d1",
     "pictureUrl":"http://obs.line-apps.com/...",
     "statusMessage":"Hello, LINE!"
 }
