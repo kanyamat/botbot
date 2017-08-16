ea5d8f8b71176beef10cb0300b05e17fd979db8d
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-15 21:57:37 -0700

    Update runhello.php

--- runhello.php
+++ runhello.php
@@ -1,8 +1,8 @@
 <?php
-$json = file_get_contents('hello.json');
-$data = json_decode($json,true);
-$message = $data['events'][0]['message']['text'];
-?>
-<div>
-<?php echo $message ?>
-</div>
+
+{
+    "displayName":"LINE taro",
+    "userId":"Uxxxxxxxxxxxxxx...",
+    "pictureUrl":"http://obs.line-apps.com/...",
+    "statusMessage":"Hello, LINE!"
+}
