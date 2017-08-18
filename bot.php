3945976926c9aaf557e88dacb2946ce5cf4ac966
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-17 21:52:02 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -53,14 +53,15 @@ if (!is_null($events['events'])) {
 // //   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 // }
   }else{
-   $arrPostData = array();
+   $event['type'] == 'message'
+//    $arrPostData = array();
    $replyToken = $event['replyToken'];
-//     $messages = [
-//      'type' => 'text',
-//      'text' => "พิมพ์ใหม่อีกทีนะ"
-//      ]; 
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "พิมพ์ใหม่อีกทีนะ";
+    $messages = [
+     'type' => 'text',
+     'text' => "พิมพ์ใหม่อีกทีนะ"
+     ]; 
+//   $arrPostData['messages'][0]['type'] = "text";
+//   $arrPostData['messages'][0]['text'] = "พิมพ์ใหม่อีกทีนะ";
   } 
 }
 }
