97c2164648154a94dbd65f18615568855fa50922
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-20 18:55:25 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
      'text' => "เราชื่อ botbot นะ"
      ];
    
-  }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "butt") ){
+  }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "butt"){
     $replyToken = $event['replyToken']; 
     $messages = [
    
