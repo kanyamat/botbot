5b8aa8e43555356a7352bc5f7ce218ce16771f4d
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-20 20:36:57 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -82,7 +82,7 @@ if (!is_null($events['events'])) {
   
   }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "con"){
     $replyToken = $event['replyToken']; 
-    $change = strtolower($event);
+    $text = strtolower($event);
     $messages = [ 
   'type'=> 'template',
   'altText'=> 'this is a confirm template',
