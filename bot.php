80b0653418526c70287d5ecc89a92e55eea308d7
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-20 19:22:20 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -79,8 +79,29 @@ if (!is_null($events['events'])) {
   ]
 
 ];  
-   
   
+  }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "con"){
+    $replyToken = $event['replyToken']; 
+    $messages = [ 
+  'type'=> 'template',
+  'altText'=> 'this is a confirm template',
+  'template'=> [
+      'type'=> 'confirm',
+      'text'=> 'Are you sure?',
+      'actions'=> [
+          [
+            'type'=> 'message',
+            'label'=> 'Yes',
+            'text'=> 'yes'
+          ],
+          [
+            'type'=> 'message',
+            'label'=> 'No',
+            'text'=> 'no'
+          ]
+      ]
+  ]
+];  
   }else{
    $replyToken = $event['replyToken'];
    $text = "พิมพ์ใหม่อีกทีนะ";
