263a5fb6f71b367764f2b45d4e41b873801c793a
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-20 18:48:25 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,6 +37,39 @@ if (!is_null($events['events'])) {
      'text' => "เราชื่อ botbot นะ"
      ];
    
+  }else if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "butt") ){
+    $replyToken = $event['replyToken']; 
+   
+   {
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
+   
+   
+   
   }else{
    $replyToken = $event['replyToken'];
    $text = "พิมพ์ใหม่อีกทีนะ";
