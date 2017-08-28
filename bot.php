23e51fc0481ac95509f40d58fe154562d8d66d59
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-27 21:11:35 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -210,12 +210,12 @@ if (!is_null($events['events'])) {
    }else if (strpos($_msg, 'ค้นหา') !== false) {
     $replyToken = $event['replyToken'];
     $x_tra = str_replace("ค้นหา","", $_msg);
-    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
-    $json= file_get_contents($url);
-    $events = json_decode($json, true);
-    $title= $events['items'][0]['title'];
-    $link = $events['items'][0]['link']; 
-    $link2 = $events['items'][1]['link'];
+//     $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
+//     $json= file_get_contents($url);
+//     $events = json_decode($json, true);
+//     $title= $events['items'][0]['title'];
+//     $link = $events['items'][0]['link']; 
+//     $link2 = $events['items'][1]['link'];
      $messages = [ 
    
   'type'=> 'template',
