967b3e3883720fe813b2174bfd9b70b3f37a9648
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-27 19:01:16 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -196,19 +196,44 @@ if (!is_null($events['events'])) {
 // // </script>
 // <gcse:search></gcse:search>
 
-  }else{
-   $replyToken = $event['replyToken'];
-   $text = "พิมพ์ใหม่อีกทีนะ";
+  }else if (strpos($_msg, 'แปล') !== false) {
+	$replyToken = $event['replyToken']; 
+	
+
+    $x_tra = str_replace("แปล","", $_msg);
+//    $url = 'http://search.pantip.com/ss?s=a&nms=1&sa=Smart+Search&q='.$x_tra;
+  //    $url = 'https://www.google.co.th/search?source=hp&q='.$x_tra;
+    
+    	$myfile = fopen("ThToEn.php", "r") or die("Unable to open file!");
+	$readfile = fread($myfile,filesize("ThToEn.php"));
+	fclose($myfile);
+	  
     $messages = [
-     'type' => 'text',
-     'text' => $text
-     ]; 
+          'type' => 'text',
+          'text' =>  $readfile 
+        ];
+	$obj = json_decode(file_get_contents($readfile), true);
+	echo $obj['access_token'];
+	      $messages = [
+          'type' => 'text',
+          'text' =>  $obj 
+        ];	
+	  
+	
+
+	  
+	  
+	  
+  }else{
+	   $replyToken = $event['replyToken'];
+	   $text = "พิมพ์ใหม่อีกทีนะ";
+	    $messages = [
+	     'type' => 'text',
+	     'text' => $text
+	     ]; 
   } 
 }
 }
-
-		
-
 
 
 // echo "OK"; 
