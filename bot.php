ff6bbcc50b05ad29cde3960469e3e968d965a1a7
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-27 21:16:24 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -166,46 +166,46 @@ if (!is_null($events['events'])) {
 ]; 
    
    
-  }else if (strpos($_msg, 'หา') !== false) {
-    $replyToken = $event['replyToken'];
-    $x_tra = str_replace("หา","", $_msg);
-    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
-    $json= file_get_contents($url);
-    $events = json_decode($json, true);
-    $title= $events['items'][0]['title'];
-    $link = $events['items'][0]['link'];
-    $link2 = $events['items'][1]['link'];
-   // $re = $events['kind'];
-   //          $messages = [
-   //            'type' => 'text',
-   //            'text' =>  $title
-   //          ];
-   $messages = [
-        'type' => 'template',
-        'altText' => 'template',
-        'template' => [
-            'type' => 'buttons',
-            'title' =>  $x_tra,
-            'text' =>   $title,
-            'actions' => [
-                [
-                    'type' => 'postback',
-                    'label' => 'good',
-                    'data' => 'value'
-                ],
-                [
-                    'type' => 'uri',
-                    'label' => 'ไปยังลิงค์',
-                    'uri' => $link
-                ],
-		                [
-                    'type' => 'uri',
-                    'label' => 'ไปยังลิงค์2',
-                    'uri' => $link2
-                ]
-            ]
-        ]
-    ];
+//   }else if (strpos($_msg, 'หา') !== false) {
+//     $replyToken = $event['replyToken'];
+//     $x_tra = str_replace("หา","", $_msg);
+//     $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
+//     $json= file_get_contents($url);
+//     $events = json_decode($json, true);
+//     $title= $events['items'][0]['title'];
+//     $link = $events['items'][0]['link'];
+//     $link2 = $events['items'][1]['link'];
+//    // $re = $events['kind'];
+//    //          $messages = [
+//    //            'type' => 'text',
+//    //            'text' =>  $title
+//    //          ];
+//    $messages = [
+//         'type' => 'template',
+//         'altText' => 'template',
+//         'template' => [
+//             'type' => 'buttons',
+//             'title' =>  $x_tra,
+//             'text' =>   $title,
+//             'actions' => [
+//                 [
+//                     'type' => 'postback',
+//                     'label' => 'good',
+//                     'data' => 'value'
+//                 ],
+//                 [
+//                     'type' => 'uri',
+//                     'label' => 'ไปยังลิงค์',
+//                     'uri' => $link
+//                 ],
+// 		                [
+//                     'type' => 'uri',
+//                     'label' => 'ไปยังลิงค์2',
+//                     'uri' => $link2
+//                 ]
+//             ]
+//         ]
+//     ];
 
    }else if (strpos($_msg, 'ค้นหา') !== false) {
     $replyToken = $event['replyToken'];
