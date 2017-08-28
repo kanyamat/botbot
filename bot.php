8e542c94be965e4ada0416606d5aff2e543e72ff
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-27 20:33:43 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -169,7 +169,7 @@ if (!is_null($events['events'])) {
   }else if (strpos($_msg, 'หา') !== false) {
     $replyToken = $event['replyToken'];
     $x_tra = str_replace("หา","", $_msg);
-    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0'.$x_tra;
+    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0'.$x_tra;
     $json= file_get_contents($url);
     $events = json_decode($json, true);
     $title= $events['items'][0]['title'];
