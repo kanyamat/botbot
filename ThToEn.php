642b75e563340b65bed6f6c036bb565dcaf22163
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-08-27 18:42:29 -0700

    Update ThToEn.php

--- ThToEn.php
+++ ThToEn.php
@@ -2,7 +2,7 @@
  require_once "GoogleTrans.php";
  $word = $_REQUEST['word'];
 $GT = NEW GoogleTranslate();
-$response = $GT->translate('th','en',$word);  /// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้
+$response = $GT->translate('th','en',$word);  
 //echo "<pre>";
 echo $word."   =   ".$response;
 ?>
