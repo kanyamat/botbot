922fc474af66c9328e46ac01d504d51962b9c8ac
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-01 01:32:06 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,41 +43,41 @@ if (!is_null($events['events'])) {
         'text' => $text
       ]; 
 
-  }else if (strpos($_msg, 'find') !==false){
+//   }else if (strpos($_msg, 'find') !==false){
 	  
-       $replyToken = $event['replyToken'];
-       $x_tra = str_replace("find","", $_msg);
-       $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
-       $json= file_get_contents($url);
-       $events = json_decode($json, true);
-        //$z = [];
- 	  //$z = array(
-		  for ($x = 0; $x <= 4; $x++) {
-		    $title= $events['items'][$x]['title'];
-		    $link = $events['items'][$x]['link'];
-		} );
-//           array_push($z);  
+//        $replyToken = $event['replyToken'];
+//        $x_tra = str_replace("find","", $_msg);
+//        $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
+//        $json= file_get_contents($url);
+//        $events = json_decode($json, true);
+//         //$z = [];
+//  	  //$z = array(
+// 		  for ($x = 0; $x <= 4; $x++) {
+// 		    $title= $events['items'][$x]['title'];
+// 		    $link = $events['items'][$x]['link'];
+// 		} );
+// //           array_push($z);  
 	  
-	  	$myObj->$title = $events['items'][$x]['title'];
-		$myObj->$link = $events['items'][$x]['link'];
+// 	  	$myObj->$title = $events['items'][$x]['title'];
+// 		$myObj->$link = $events['items'][$x]['link'];
 		
-		$myJSON = json_encode($myObj);
+// 		$myJSON = json_encode($myObj);
 
-		//echo $myJSON;
+// 		//echo $myJSON;
 
-// 	$i = 0; 
+// // 	$i = 0; 
 
-// 	while($i <= 4) {
-// 	    $title= $events['items'][$i]['title'];
-// 	    $link = $events['items'][$i]['link'];
-// 	    $i++;
-// 	} 
-	   $messages = [
+// // 	while($i <= 4) {
+// // 	    $title= $events['items'][$i]['title'];
+// // 	    $link = $events['items'][$i]['link'];
+// // 	    $i++;
+// // 	} 
+// 	   $messages = [
 
- 		'type' => 'text',
-        	'text' => $myJSON
+//  		'type' => 'text',
+//         	'text' => $myJSON
 
-	     ];
+// 	     ];
 	  
 
   }else if ($event['type'] == 'message' && $event['message']['text'] == "ชื่ออะไร"){
