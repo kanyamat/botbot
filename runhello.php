b7edf0d2cdc9d08a6b0f1dfd1b6e3ccc163904e1
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-07 01:26:29 -0700

    Delete runhello.php

--- runhello.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-{
-    "displayName":"LINE taro",
-    "userId":"Ub840b452d253f3db490dd59507ab78d1",
-    "pictureUrl":"http://obs.line-apps.com/...",
-    "statusMessage":"Hello, LINE!"
-}
