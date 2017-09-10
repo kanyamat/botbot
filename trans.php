6e4d5472fe736cd59ef6ea38d4718ce94e9983b0
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-10 16:01:29 +0700

    Update trans.php

--- trans.php
+++ trans.php
@@ -3,6 +3,7 @@
  $word = $_REQUEST['word'];
 $GT = NEW GoogleTranslate();
 $response = $GT->translate('th','en',$word);  /// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้
-//echo "<pre>";
+
 echo $word."   =   ".$response;
+
 ?>
