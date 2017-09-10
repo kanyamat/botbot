f7fc0b1e4e921f926d062b61a5c40cdd682ab7ba
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-10 15:49:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -208,7 +208,7 @@ $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_g
                 [
                     'type' => 'uri',
                     'label' => 'trans',
-                    'uri' => 'https://botbot1234.herokuapp.com/trans.php?word='$x_tra
+                    'uri' => 'https://botbot1234.herokuapp.com/trans.php?word='.$x_tra
                 ]
             ]
         ]
