289ad431539c0e9a779d6cabb14581b3f6327c94
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-11 20:30:02 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -174,7 +174,7 @@ $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_g
 
 	  
        for ($i = 0 ; $i<5 ; $i++){
-            $me[$i] = [
+            $me[$i] = [[[
                                 'title' => $events['items'][$i]['title'],
                                 'text' => 'description',
                                 'actions' => [
@@ -189,7 +189,7 @@ $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_g
                                         'uri' => $events['items'][$i]['link']
                                     ]
                                 ]
-                             ];
+                             ]]];
   
           }  
                $messages = [
