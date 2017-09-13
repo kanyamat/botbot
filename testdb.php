52a42bb2a3ec44356a2a73e44ea862d4efe449f9
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-12 21:52:05 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -13,8 +13,7 @@ $sql = "CREATE TABLE Test (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
+email VARCHAR(50)
 )";
 
 if ($conn->query($sql) === TRUE) {
