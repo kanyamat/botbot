d9dfcb78bd5bf862c65a7701de610d24e48a7ec3
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-12 21:50:53 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -6,6 +6,27 @@ $dbconn = pg_connect($conn_string);
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
+
+// sql to create table
+$sql = "CREATE TABLE Test (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+
 ?>
+
+
 
