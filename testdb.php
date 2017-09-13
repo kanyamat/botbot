7cf740178dc7d6980b9c32ad374fba301fa0b57c
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-12 20:53:15 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -3,7 +3,7 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.comp port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47";
 $dbconn4 = pg_connect($conn_string);
 
-if (!$conn_string) {
+if (!$dbconn4) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
