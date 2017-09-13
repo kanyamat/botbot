155df6d2eeb2396428103a2f8dbac0f7b366ddd6
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-13 00:25:04 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -8,21 +8,13 @@ if (!$dbconn) {
 }
 echo "Connected successfully";
 
-// sql to create table
-// $sql = "CREATE TABLE Test (
-// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-// firstname VARCHAR(30) NOT NULL,
-// lastname VARCHAR(30) NOT NULL,
-// email VARCHAR(50)
-// )";
-
-// if ($conn->query($sql) === TRUE) {
-//     echo "Table MyGuests created successfully";
-// } else {
-//     echo "Error creating table: " . $conn->error;
-// }
-
-// $conn->close();
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
 
 
 ?>
