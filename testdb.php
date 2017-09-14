d5529b7d06115fbbef3ee218a2b485227b45b731
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-13 19:09:35 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -3,13 +3,19 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47";
 $dbconn = pg_connect($conn_string);
 
-if (!$dbconn) {
-    die("Connection failed: " . mysqli_connect_error());
-}else
+// if (!$dbconn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }else
 
+// echo " successfully";
 
-
-echo " successfully";
+$sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
+userID VARCHAR (100),
+date_history DATE,
+width DOUBLE,
+height DOUBLE,
+PRIMARY KEY(History_ID)
+)";
 
 ?>
 
