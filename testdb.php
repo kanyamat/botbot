93d0f7a3ca0e7e605d2974816331edeb925f9d5f
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-13 19:11:36 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -3,9 +3,9 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47";
 $dbconn = pg_connect($conn_string);
 
-// if (!$dbconn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }else
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
 
 // echo " successfully";
 
@@ -16,7 +16,7 @@ width DOUBLE,
 height DOUBLE,
 PRIMARY KEY(History_ID)
 )";
-
+}
 ?>
 
 
