69ffafbd55fdf888926b9c2ef79922966839b9e5
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-13 20:53:24 -0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -3,9 +3,9 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 schemas=bot ";
 $dbconn = pg_connect($conn_string);
 
-// if (!$dbconn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 
 // $sql = "CREATE TABLE bot.History(History_ID INT NOT NULL AUTO_INCREMENT,
@@ -18,7 +18,7 @@ $dbconn = pg_connect($conn_string);
     
 //     $dbconn2 = pg_connect($sql);
     
-//  echo " successfully";
+ echo " successfully";
 
 $ResId = pg_exec("CREATE TABLE bot.MyGuests", $dbconn);
 ?>
