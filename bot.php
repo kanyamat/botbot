23f8dfe8ab6d41c8e31f155396e6aeae087af1be
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-14 23:54:02 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -290,13 +290,13 @@ $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_g
     $weight  = str_replace("","",$pieces[1]);
 //********ใส่ 5 ค่าลง array********//
 
-$user = $events['events'][0]['source']['userId'];
-$b = [$user];
+//$user = $events['events'][0]['source']['userId'];
+
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 $dbconn = pg_pconnect($conn_string);
 
 	  
-$sql="INSERT INTO History(userid,date_history,weight,height) VALUES($b,NOW(),$weight,$height)";
+$sql="INSERT INTO History(userid,date_history,weight,height) VALUES($events['events'][0]['source']['userId'],NOW(),$weight,$height)";
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 	
 
   }else{
