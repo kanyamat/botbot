b22d64c596e33ef6e044eedb1cf387b63d8a3472
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-18 01:25:10 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -307,13 +307,31 @@ pg_exec($dbconn, $sql) or die(pg_errormessage());
 	$conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 	$dbconn = pg_pconnect($conn_string);
         $sql =  "SELECT height FROM history";
-	$a = pg_exec($dbconn, $sql); 
-	//$a = pg_fetch_assoc($dbconn, $sql); 
+	//$a = pg_exec($dbconn, $sql); 
+	$a = pg_query($dbconn, $sql); 
+	  
+	while ($row = pg_fetch_row($a)) {
+  	echo "$row[0] $row[1] $row[2]\n";
+	}
+
+	pg_close($con); 
+	  
+	  
 	    $messages = [
 	     'type' => 'text',
-	     'text' => $a
+	     'text' => $row
 	     ]; 
 	 
+	  
+	  
+	  
+	  $query = "SELECT height FROM history"; 
+
+$rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+
+
+	  
+	  
 	  
 	  
   }else{
