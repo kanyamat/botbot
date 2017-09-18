ef7fffca2ddd65f9542e410dc711344caca5b4c5
kanyamat <31055915+kanyamat@users.noreply.github.com>
2017-09-17 23:47:13 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -306,11 +306,11 @@ pg_exec($dbconn, $sql) or die(pg_errormessage());
       $replyToken = $event['replyToken'];
 	$conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 	$dbconn = pg_pconnect($conn_string);
-        $sql = "SELECT weight FROM history";
+        $sql = "SELECT weight FROM public.history";
 	$result = pg_exec($dbconn, $sql) or die(pg_errormessage()); 
 	    $messages = [
 	     'type' => 'text',
-	     'text' => $result
+	     'text' => $sql
 	     ]; 
 	 
 	  
